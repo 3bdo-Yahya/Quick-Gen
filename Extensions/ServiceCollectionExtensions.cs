eef1274ff2d4894f0d97c05d97be70d36f703cc6
yousefalaa <yousefalaamohammed4@gmail.com>
2026-04-19 17:10:20 +0200

    implement course CRUD operations

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -37,6 +37,7 @@ public static class ServiceCollectionExtensions
             .AddDefaultTokenProviders();
 
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<ICourseService, CourseService>();
 
         return services;
     }
---
+++ Services/IServices/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using Quick_Gen.Contracts.Courses;
+
+namespace Quick_Gen.Services.IServices
+{
+    public interface ICourseService
+    {
+        Task<IEnumerable<CourseResponse>> GetAllAsync();
+        Task<CourseResponse?> GetByIdAsync(int id);
+        Task<CourseResponse> CreateAsync(CreateCourseRequest request);
+        Task<CourseResponse?> UpdateAsync(int id, UpdateCourseRequest request);
+        Task<bool> DeleteAsync(int id);
+    }
+}
